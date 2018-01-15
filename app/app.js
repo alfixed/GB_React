9a79757718e003ecf859f563b9c9001ee41ce716
Aleksei Fedosimov <alfixed@Alekseis-MacBook-Pro.local>
2018-01-15 20:47:52 +0300

    Add onClick

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class App extends React.Component
 {
     render(){
-        return <h1>First app on React</h1>;
+        return <h1 onClick={alert('1')}>First app on React</h1>;
     }
 }
 
